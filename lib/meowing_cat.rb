1afdcb6499c750e2925b4c643f78167bd846851b
Anthony Risch <rischthefisch@gmail.com>
2020-02-07 00:26:44 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,18 +1,3 @@
 class Cat 
-  attr_accessor :name 
+  attr_accessible :name 
 end
-
- def meow=(hiss)
-   @me = hiss
-  end
-  
-  def meow 
-    puts meow
-  end
-end
-
-maru = Cat.new 
-maru.name = "Maru"
-
-maru.name
-maru.meow
